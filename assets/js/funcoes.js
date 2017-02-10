d7be6a7ff3d949cff9a91f36711e1f80983b904b
ArthurLDS <arthurlds73@gmail.com>
2017-02-10 19:47:40 -0200

    feat(funcoesJS): implementar listagem de pessoas.

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -13,6 +13,8 @@ coffeChallange.iniciar = function(){
     if(coffeChallange.pessoas == null){
       coffeChallange.pessoas = [];
     }
+    coffeChallange.listaPessoas = $("#listaPessoas");
+    coffeChallange.listarPessoas();
     coffeChallange.configurarBtns();
 };
 
@@ -22,11 +24,30 @@ coffeChallange.configurarBtns = function(){
 }
 
 coffeChallange.adicionar = function(){
+  if(validacao.validarCampoNome()){
     let pessoa = JSON.stringify({
           nome : coffeChallange.$txtNome.val()
     });
 
     coffeChallange.pessoas.push(pessoa);
     localStorage.setItem("pessoas", JSON.stringify(coffeChallange.pessoas));
-    alert(JSON.parse(pessoa).nome + " cadastrado(a) com SUCESSO!");
+    coffeChallange.listarPessoas();
+  }
 };
+
+coffeChallange.listarPessoas = function(){
+  coffeChallange.listaPessoas.html("");
+
+	for(var i in coffeChallange.pessoas){
+		let pessoa = JSON.parse(coffeChallange.pessoas[i]);
+    coffeChallange.listaPessoas.append("<tr><td>"+pessoa.nome+"</td><td><a href='javascript:coffeChallange.excluirPessoa("+i+")'><i class='fa fa-trash-o' aria-hidden='true'></i></a></td></tr>");
+
+	}
+}
+
+coffeChallange.excluirPessoa = function(id){
+   let pessoa = JSON.parse(coffeChallange.pessoas[id]);
+   coffeChallange.pessoas.splice(id, 1);
+   localStorage.setItem("pessoas", JSON.stringify(coffeChallange.pessoas));
+   coffeChallange.listarPessoas();
+}
