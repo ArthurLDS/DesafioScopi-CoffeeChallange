e564d17186aff96e93e67061b7630b9612f318eb
ArthurLDS <arthurlds73@gmail.com>
2017-02-12 21:06:19 -0200

    refactor(validacoesJS): refatorar notificação de erro.

--- assets/js/validacoes.js
+++ assets/js/validacoes.js
@@ -3,11 +3,13 @@ var validacao = {}
 $(function(){
    validacao.$notificacaoNonePessoas = $('#notificacao-none-pessoas');
 });
-validacao.notificarErro = function(mostrar){
+validacao.notificarErro = function(mostrar, mensagem){
   let alerta = $('#alert-erro');
+  let alertaMsg = $('#alert-erro spam');
 
   if(mostrar){
     alerta.show();
+    alertaMsg.text(mensagem);
     setTimeout(function () {
       alerta.fadeOut("slow");
     }, 8000);
@@ -19,26 +21,21 @@ validacao.notificarErro = function(mostrar){
 
 validacao.validarCampoNome = function(){
   let valido;
-  let alertaMsg = $('#alert-erro spam');
-
   for(var i in coffechallenge.pessoas){
     if(JSON.parse(coffechallenge.pessoas[i]).nome === coffechallenge.$txtNome.val()){
-      validacao.notificarErro(true);
-      alertaMsg.text("Este nome já existe!");
+      validacao.notificarErro(true, "Este nome já existe!");
       return false;
     }
   }
   if(coffechallenge.$txtNome.val())
     validacao.notificarErro(false);
   else{
-    alertaMsg.text("O campo nome não pode estar vazio!");
-    validacao.notificarErro(true);
+    validacao.notificarErro(true, "O campo nome não pode estar vazio!");
     return false;
   }
 
   if(coffechallenge.$txtNome.val().length <3){
-    validacao.notificarErro(true);
-    alertaMsg.text("O campo nome deve ter no mínimo 3 letras");
+    validacao.notificarErro(true, "O campo nome deve ter no mínimo 3 letras");
     return false;
   }
   return true;
