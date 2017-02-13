69b7243268b5052517d75ddb27d704a30f6f9851
ArthurLDS <arthurlds73@gmail.com>
2017-02-13 20:41:07 -0200

    refactor(validacoesJS): dividir regra de negóci  de validação de nome em métodos separados.

--- assets/js/validacoes.js
+++ assets/js/validacoes.js
@@ -1,58 +1,76 @@
 var validacao = {}
 
 $(function(){
-   validacao.$notificacaoNonePessoas = $('#notificacao-none-pessoas');
+  validacao.$notificacaoNenhumaPessoa = $('#notificacao-nenhuma-pessoa');
 });
 
-validacao.notificarErro = function(mostrar, mensagem){
+validacao.notificarErroNomePessoa = function(mostrar, mensagem){
   let alerta = $('#alert-erro');
   let alertaMsg = $('#alert-erro spam');
 
   if(mostrar){
     alerta.show();
     alertaMsg.text(mensagem);
-    setTimeout(function () {
-      alerta.fadeOut("slow");
-    }, 8000);
   }
   else {
-    alerta.fadeOut("slow");
+    alerta.fadeOut("fast");
   }
 }
 
-validacao.validarCampoNome = function(){
-  let valido;
+validacao.validarNomePessoa = function(){
+  if(validacao.nomePessoaExistencia() && validacao.nomePessoaVazio() && validacao.nomePessoaTamanho()){
+    validacao.notificarErroNomePessoa(false);
+    return true;
+  }
+  return false;
+}
+
+validacao.nomePessoaExistencia = function(){
   for(var i in coffechallenge.pessoas){
     if(JSON.parse(coffechallenge.pessoas[i]).nome === coffechallenge.$txtNome.val()){
-      validacao.notificarErro(true, "Este nome já existe!");
+      validacao.notificarErroNomePessoa(true, "Este nome já existe!");
       return false;
     }
   }
-  if(coffechallenge.$txtNome.val())
-    validacao.notificarErro(false);
-  else{
-    validacao.notificarErro(true, "O campo nome não pode estar vazio!");
-    return false;
-  }
+  return true;
+}
 
-  if(coffechallenge.$txtNome.val().length <3){
-    validacao.notificarErro(true, "O campo nome deve ter no mínimo 3 letras");
+validacao.nomePessoaVazio = function(){
+  if(coffechallenge.$txtNome.val()){
+    return true;
+  }
+  validacao.notificarErroNomePessoa(true, "O nome não pode estar vazio!");
+  return false;
+}
+
+validacao.nomePessoaTamanho = function(){
+  if(coffechallenge.$txtNome.val().length < 3){
+    validacao.notificarErroNomePessoa(true, "O nome deve ter no mínimo 3 letras");
     return false;
   }
   return true;
 }
 
 validacao.validarListaPessoas = function(){
-  let linkPageSorteio = "sorteioCoffeeMakersList.html";
 
-  if(coffechallenge.pessoas === null || coffechallenge.pessoas.length === 0 || coffechallenge.pessoas.length === ""){
-    validacao.$notificacaoNonePessoas.show();
+  if(coffechallenge.pessoas === null || coffechallenge.pessoas.length === 0 || coffechallenge.pessoas === ""){
+    validacao.notificarErroListaPessoas(true);
+    return false;
+  }
+  validacao.notificarErroListaPessoas(false);
+  return true;
+}
+
+validacao.notificarErroListaPessoas = function(mostrar){
+  let linkPageSorteio = "sorteioCoffeeMakersList.html";
+  if(mostrar){
+    validacao.$notificacaoNenhumaPessoa.show();
     coffechallenge.$contentPessoas.hide();
     coffechallenge.$btnGerarCoffeMakers.attr("disabled", true);
     coffechallenge.$btnGerarCoffeMakers.attr("href", "#");
   }
   else{
-    validacao.$notificacaoNonePessoas.hide();
+    validacao.$notificacaoNenhumaPessoa.hide();
     coffechallenge.$contentPessoas.show();
     coffechallenge.$btnGerarCoffeMakers.attr("disabled", false);
     coffechallenge.$btnGerarCoffeMakers.attr("href", linkPageSorteio);
